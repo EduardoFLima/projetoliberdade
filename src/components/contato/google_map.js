c32371d56c8d8ff18300a0aab80050cd10a1d7de
Edu Lima <eduefi@gmail.com>
2025-05-31 16:47:01 +0200

    make map search parameter flexible

--- src/components/contato/google_map.js
+++ src/components/contato/google_map.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class GoogleMap extends Component {
 
     render() {
-        const { lat, lon, title, zoom } = this.props;
+        const { title, searchParameter, zoom } = this.props;
 
         return <iframe
-         src={`https://www.google.com/maps?q=${lat},${lon}&z=${zoom}&output=embed&language=pt-br`}
+         src={`https://www.google.com/maps?q=(${searchParameter})&z=${zoom}&output=embed&language=pt-br`}
             style={this.props.style}
             frameBorder="0" 
             title={title}
