ea1f9698b79bb921f743cfe592acb446717afc68
Eduardo Lima <eduefi@gmail.com>
2026-07-02 00:06:21 +0200

    feat: add Organic Freedom design tokens and fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router/dom'
 import { router } from './routes'
 import './index.css'
+import '@fontsource/plus-jakarta-sans/600.css'
+import '@fontsource/plus-jakarta-sans/700.css'
+import '@fontsource/work-sans/400.css'
+import '@fontsource/work-sans/500.css'
+import '@fontsource/work-sans/600.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
